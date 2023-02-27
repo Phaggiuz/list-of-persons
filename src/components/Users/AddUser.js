53b0ae193963a9de7cb71e9488fffae0201babdd
Phaggiuz <francesco.faggioni.1994@gmail.com>
2023-02-27 14:01:26 +0100

    adding new Card component, adding style modules in Card and AddUser components

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,18 +1,23 @@
 import React from "react";
 
+import Card from "../UI/Card";
+import styles from "./AddUser.module.css";
+
 const AddUser = (props) => {
   const addUserHandler = (event) => {
     event.preventDefault();
   };
 
   return (
-    <form onSubmit={addUserHandler}>
-      <label htmlFor="username">Username</label>
-      <input id="username" type="text" placeholder="username"></input>
-      <label htmlFor="age">Age (Year)</label>
-      <input id="age" type="number" placeholder="0"></input>
-      <button type="submit">Add User</button>
-    </form>
+    <Card className={styles.input}>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor="username">Username</label>
+        <input id="username" type="text" placeholder="username"></input>
+        <label htmlFor="age">Age (Year)</label>
+        <input id="age" type="number" placeholder="0"></input>
+        <button type="submit">Add User</button>
+      </form>
+    </Card>
   );
 };
 
