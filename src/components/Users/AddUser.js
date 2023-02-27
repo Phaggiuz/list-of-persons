f5ee2f0fa9673da3cf8db319cc643500e6a8cf69
Phaggiuz <francesco.faggioni.1994@gmail.com>
2023-02-27 15:27:57 +0100

    adding states variables for managing user input values

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,21 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Card from "../UI/Card";
 import Button from "../UI/Button";
 import styles from "./AddUser.module.css";
 
 const AddUser = (props) => {
+  const [enteredUsername, setEnteredUsername] = useState("");
+  const [enteredAge, setEnteredAge] = useState("");
+
   const addUserHandler = (event) => {
     event.preventDefault();
+    console.log(`Username: ${enteredUsername}, Age: ${enteredAge}`);
+  };
+
+  const usernameChangeHandler = (event) => {
+    setEnteredUsername(event.target.value);
+  };
+  const ageChangeHandler = (event) => {
+    setEnteredAge(event.target.value);
   };
 
   return (
     <Card className={styles.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input id="username" type="text" placeholder="username"></input>
+        <input
+          id="username"
+          type="text"
+          placeholder="username"
+          onChange={usernameChangeHandler}
+        ></input>
         <label htmlFor="age">Age (Year)</label>
-        <input id="age" type="number" placeholder="0"></input>
+        <input
+          id="age"
+          type="number"
+          placeholder="0"
+          onChange={ageChangeHandler}
+        ></input>
         <Button type="submit">Add User</Button>
       </form>
     </Card>
